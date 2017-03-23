c004a8f65120e741912df20b431929ee36f35912
Robert Onesim <robert.onesim@gmail.com>
2017-03-23 23:30:48 +0200

    Optimization

--- homework5/app5.py
+++ homework5/app5.py
@@ -17,13 +17,27 @@ def generateV0(A):
     return A.transpose() / (np.max(np.sum(A, axis=0)) * np.max(np.sum(A, axis=1)))
 
 
-def inverseSchulzMethod(A, n, eps, kMax):
+def addElemDiagonal(matrix, value):
+    for row in range(len(matrix)):
+        matrix[row, row] += value
+    return np.matrix(matrix)
+
+
+def removeElemDiagonal(matrix, value):
+    for row in range(len(matrix)):
+        matrix[row, row] -= value
+    return np.matrix(matrix)
+
+
+def inverseSchulzMethod(A, B, n, eps, kMax):
     # initialize V0
     V0 = generateV0(A)
     # V(k+1) = V(k) *(2*I(n) - A*V(k))
     while kMax > 0:
         # compute V1 using V0
-        V1 = V0 * (2 * np.identity(n) + (-1) * A * V0)
+        C = B * V0
+        # add 2 on C's diagonal and multiply with V0
+        V1 = V0 * addElemDiagonal(C, 2)
         # check exit condition
         norm = LA.norm(V1 - V0)
         if norm < eps:
@@ -37,13 +51,15 @@ def inverseSchulzMethod(A, n, eps, kMax):
     return "Divergent", 0
 
 
-def inverseLiMethodI(A, n, eps, kMax):
+def inverseLiMethodI(A, B, n, eps, kMax):
     # initialize V0
     V0 = generateV0(A)
     # V(k+1) = V(k) *(3*I(n) - A*V(k)(3*I(n) - A*V(k)))
     while kMax > 0:
         # compute V1 using V0
-        V1 = V0 * (3 * np.identity(n) + (((-1) * A * V0) * (3 * np.identity(n) + (-1) * A * V0)))
+        C = B * V0
+        auxMat = addElemDiagonal(C, 3)
+        V1 = V0 * addElemDiagonal(removeElemDiagonal(C, 3) * auxMat, 3)
         # check exit condition
         norm = LA.norm(V1 - V0)
         if norm < eps:
@@ -57,14 +73,15 @@ def inverseLiMethodI(A, n, eps, kMax):
     return "Divergent", 0
 
 
-def inverseLiMethodII(A, n, eps, kMax):
+def inverseLiMethodII(A, B, n, eps, kMax):
     # initialize V0
     V0 = generateV0(A)
     # V(k+1) = (I(n) + 1/4 * (I(n) - V(k) *A)(3*I(n) - V(k) *A)^2) *V(k)
     while kMax > 0:
         # compute V1 using V0
-        auxV = 3 * np.identity(n) + (-1) * A * V0
-        V1 = V0 * (np.identity(n) + ((np.identity(n) + (-1) * A * V0) * auxV * auxV) / 4)
+        C = V0 * B
+        auxMat = addElemDiagonal(C, 3) ** 2
+        V1 = addElemDiagonal(removeElemDiagonal(C, 2) * auxMat / 4, 1) * V0
         # check exit condition
         norm = LA.norm(V1 - V0)
         if norm < eps:
@@ -80,9 +97,12 @@ def inverseLiMethodII(A, n, eps, kMax):
 
 matrix, n, eps, kMax = readFile(sys.argv[1])
 
-matrixInvSchulz, iter = inverseSchulzMethod(matrix, n, eps, kMax)
+# compute B = -matrix: used in C = a*I(n) - A * V
+B = np.matrix((-1) * matrix)
+
+matrixInvSchulz, iter = inverseSchulzMethod(matrix, B, n, eps, kMax)
 print("Schultz method:")
-if matrixInvSchulz == "Divergent":
+if type(matrixInvSchulz) == str and matrixInvSchulz == "Divergent":
     print("There is no inverse after {} iterations".format(kMax))
 else:
     print("Inverse found after {} iterations".format(kMax - iter))
@@ -90,9 +110,9 @@ else:
     print("Norm ||A * A^(-1) - I(n)||1 is {}".format(np.max(np.sum(matrix * matrixInvSchulz - np.identity(n)), axis=0)))
     pprint.pprint(matrixInvSchulz)
 
-matrixInvLi, iter = inverseLiMethodI(matrix, n, eps, kMax)
+matrixInvLi, iter = inverseLiMethodI(matrix, B, n, eps, kMax)
 print("LI & LI first method:")
-if matrixInvLi == "Divergent":
+if type(matrixInvLi) == str and matrixInvLi == "Divergent":
     print("There is no inverse after {} iterations".format(kMax))
 else:
     print("Inverse found after {} iterations".format(kMax - iter))
@@ -100,9 +120,9 @@ else:
     print("Norm ||A * A^(-1) - I(n)||1 is {}".format(np.max(np.sum(matrix * matrixInvLi - np.identity(n)), axis=0)))
     pprint.pprint(matrixInvLi)
 
-matrixInvLiII, iter = inverseLiMethodII(matrix, n, eps, kMax)
+matrixInvLiII, iter = inverseLiMethodII(matrix, B, n, eps, kMax)
 print("LI & LI second method:")
-if matrixInvLiII == "Divergent":
+if type(matrixInvLiII) == str and matrixInvLiII == "Divergent":
     print("There is no inverse after {} iterations".format(kMax))
 else:
     print("Inverse found after {} iterations".format(kMax - iter))
